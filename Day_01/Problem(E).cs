99c190e4b79da3df1c60b907fdfec0605ee652ca
mo2men <ma5727@fayoum.edu.eg>
2025-02-22 03:14:07 +0200

    Problem (E)

---
+++ Day_01/Problem(E).cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string r = Console.ReadLine();
+        double area = (float.Parse(r) * (float.Parse(r))) * 3.141592653;
+        Console.WriteLine(area.ToString("f9"));
+    }
+}
