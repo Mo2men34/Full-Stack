af19541813ec07db3a189d561adcdc1d66f5672d
mo2men <ma5727@fayoum.edu.eg>
2025-02-22 03:13:27 +0200

    Problem (C)

---
+++ Day_01/Problem(C).cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] arr = input.Split();
+        Console.WriteLine($"{arr[0]} + {arr[1]} = {int.Parse(arr[0]) + int.Parse(arr[1])}");
+        Console.WriteLine($"{arr[0]} * {arr[1]} = {int.Parse(arr[0]) * int.Parse(arr[1])}");
+        Console.WriteLine($"{arr[0]} - {arr[1]} = {int.Parse(arr[0]) - int.Parse(arr[1])}");
+    }
+}
